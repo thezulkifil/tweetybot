f89333ab40378d537912d5223961c3889b670fa2
thezulkifil <82689328+thezulkifil@users.noreply.github.com>
2022-08-10 22:00:25 +0500

    updated quotes

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-TAGS = ["knowledge", "philosophy", "life", "wisdom"]
+TAGS = ["knowledge", "philosophy", "life", "wisdom", "dark", "dark-humor", "offensive", "humor", "comedy", "manipulation", "insult"]
 UNSPLASH_QUERIES = TAGS + ["history", "story", "ancient", "art"]
